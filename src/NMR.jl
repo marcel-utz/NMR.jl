1f8349541c7404dfb3afd8fa8ce32a180cf62075
Marcel Utz <marcel.utz@soton.ac.uk>
2017-12-05 12:12:51 +0000

    added includes to NMR.jl

--- src/NMR.jl
+++ src/NMR.jl
@@ -1,9 +1,18 @@
 module NMR
 
-   using SpinSim
-   using PauliMatrix
+   include("SimpleGraphics.jl")
+   include("Scaling.jl")
+   include("SimplePlot.jl")
+   include("Contour.jl")
+   include("MapPlot.jl")
+   include("PauliMatrix.jl")
+   include("SpinSim.jl")
 
-   export SpinSim
+   export SimpleGraphics
+   export Scaling
+   export Contour
+   export MapPlot
    export PauliMatrix
+   export SpinSim
 
 end
