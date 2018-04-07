219fe175147e7c1367f73803e73143b9d1b58746
Marcel Utz <marcel.utz@soton.ac.uk>
2018-04-07 11:01:59 +0100

    AutoPhaseCorrect() is now ffunctional

--- src/AutoPhase.jl
+++ src/AutoPhase.jl
@@ -1,8 +1,9 @@
 module AutoPhase
 
 using NMR.DataSet
+using Optim
 
-export entropy
+export entropy, AutoPhaseCorrect
 
 ent(x) = x*log(x)
 
@@ -14,8 +15,32 @@ This quantity can be optimised with respect to zero- and first-order
 phase correction for automatic (unsupervised) phase correction.
 """
 function entropy(spect::Data1D)
-    return sum(ent.(abs.(real.(spect.dat))))/sum(abs.(real.(spect.dat)))
+    h=abs.(real.(val(spect)))
+    h/=sum(h)
+    return -sum(ent.(h))
 end
+
+# penalty(x) computes the sum of squares of all negative points in x
+function penalty(x)
+    x /= sum(abs.(x))
+    return sum([k<0.0?k*k:0.0 for k in x])
+end
+
+# this is the minimisation target for automatic phase correction
+function goalfun(x,spect,γ=1.e-5)
+    piv=(spect.istart+spect.istop)/2
+    c=PhaseCorrect(spect,Ph0=x[1],Ph1=x[2],Pivot=piv);
+    return entropy(derivative(c))+γ*penalty(real.(val(c)));
+end
+
+function AutoPhaseCorrect(spect::Data1D;verbose=false)
+  piv=(spect.istart+spect.istop)/2
+  result=optimize(x->goalfun(x,spect),[π/2,-0.1],NelderMead());
+  if verbose print(result) end;
+  pc=Optim.minimizer(result);
+  return PhaseCorrect(spect,Ph0=pc[1],Ph1=pc[2],Pivot=piv);
+end
+
 
 
 end
