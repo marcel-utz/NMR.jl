97accfdb2fc6b63b847162396a604030921ff60f
Marcel Utz <marcel.utz@gmx.net>
2021-12-04 17:41:17 +0100

    updated special module for BNF data

--- src/ProcessBNF.jl
+++ src/ProcessBNF.jl
@@ -9,19 +9,29 @@ Special processing routines for back-and-forth pumping tissue slice culture expe
 module ProcessBNF
 
 using DataFrames
+import CSV
+using Dates
+import NMR
 
 export process_spectra, generate_protocol
 
 
+"""
+    function process_fid(n,dataStore,dataLocation)
+
+process the FID contained in the file `dataStore`*`dataLocation`/`n`. 
+Returns a `Data1D` object with the processed spectrum.
+"""
 function process_fid(n,dataStore,dataLocation)
         SW = 19.8269803808675;
         SW_h= 600*SW;
-        d=NMR.readBrukerFID(dataStore * dataLocation * "$(n)/fid")[77:end];
+        d=NMR.readBrukerFID(dataStore * dataLocation * "/$(n)/fid")[77:end];
         fid=NMR.Data1D(d,0.,length(d)/SW_h);
         spect=NMR.FourierTransform(fid,SI=16384,PPM=600.,CTR=4.82,LB=1.0π)
         spect=NMR.PhaseCorrect(spect,Ph0=2.95,Ph1=-0)
         spect=NMR.cut(spect,0.,10.)
-        spect=NMR.BaseLineCorrect(spect,order=23)
+        spect=NMR.BaseLineCorrect(spect,kfactor=4.5,regions=256,order=31)
+        #spect-=NMR.medianBaseline(spect)
         return spect
 end
 
@@ -33,17 +43,19 @@ Generates a protocol csv file, which each row corresponding to a single
 NMR spectrum, in the directory designated by `fname`. 
 """
 function generate_protocol(fname)
-    cd(fname)
+    # cd(fname)
 
-    d=readdir()
+    d=readdir(fname)
 
     boxf=findfirst(x->match(r"^box-[0-9]*\.txt",x)!=nothing,d)
     protof=findfirst(x->match(r"^labsmith-[0-9]*\.txt",x)!=nothing,d)
+    
+    # @show d[[boxf,protof]]
 
     # clean up labsmith protocol file
     
-    infile=open(d[protof],"r")
-    outfile=open("protocol-raw.csv","w")
+    infile=open(fname*d[protof],"r")
+    outfile=open(fname*"protocol-raw.csv","w")
     write(outfile,"Date,Time,PumpName,FlowRate,CurrVol,TargetVol,Push,Pull,BackForthActive,OtherTarget\n")
     while !eof(infile)
         line=readline(infile)
@@ -55,17 +67,15 @@ function generate_protocol(fname)
     
     # read in temperature log
     
-    try 
-        tlograw=CSV.read(d[boxf], DataFrame,types=[String,Float64,Float64,Float64,Float64,Float64,Float64,String])
-    catch
-    end
+    
+    tlograw=CSV.read(fname*d[boxf], DataFrame,types=[String,Float64,Float64,Float64,Float64,Float64,Float64,String])
     
     tlograw.DateTime=[try DateTime(x,"dd/mm/yyyy HH:MM:SS") catch end for x in tlograw.Time]
     tlog=tlograw[tlograw.DateTime.!=nothing,:]
         
     # read in protocol 
         
-    protocol=CSV.read("protocol-raw.csv",DataFrame)
+    protocol=CSV.read(fname*"protocol-raw.csv",DataFrame)
     protocol.DateTime=Date.(protocol[!,"Date"],"dd/mm/yyyy").+protocol[!,"Time"]
         
     # look up temperature data from tlog
@@ -92,8 +102,8 @@ function generate_protocol(fname)
     deltaT=[protocol_final[k,:DateTime]-protocol_final[k-1,:DateTime] for k in 2:nrow(protocol_final)]
     prepend!(deltaT,[Minute(9)])
     protocol_final.duration = deltaT
-            
-    CSV.write("protocol-final.csv",protocol_final)
+    
+    CSV.write(fname*"protocol-final.csv",protocol_final)
             
 end
 
