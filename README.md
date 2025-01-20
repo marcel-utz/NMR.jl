8311e63ae9fe3241180005596f8c6b553d0d20be
Marcel Utz <marcel.utz@gmx.net>
2025-01-20 08:07:53 +0100

    added docs and automated deployment

--- README.md
+++
@@ -1,5 +0,0 @@
-# NMR
-This package provides a set of routines and tools for NMR spectroscopy and imaging in Julia. 
-
-(c)2018-2021 Utz Research Group
-Twitter: @utzgroup
--- test/runtests.jl
+++
@@ -1,8 +0,0 @@
-using NMR
-using Test
-
-@testset "Reading NMR Files" begin
-    f=NMR.readBrukerFID("data/10/fid")
-    @test sum(f) == 4.2727766583449766e12 + 2.393772457231664e12im 
-end
-
